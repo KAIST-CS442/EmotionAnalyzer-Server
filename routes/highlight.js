f91c99267b89fac03d97c30285d18c36ed2a7cbd
hyunsungcho <hscho1226@gmail.com>
2017-05-19 23:51:49 +0900

    API for saving highlight to DB

--- routes/highlight.js
+++ routes/highlight.js
@@ -6,6 +6,10 @@ var mongoose = require('mongoose');
 
 var Highlight = require('../models/Highlight');
 
+/*
+ * GET highlight info
+ * Query: video_id (?video_id=xxxxx)
+ */
 router.get('/', function(req, res, next) {
   Highlight.findOne({
     video_id: req.query.video_id
@@ -15,5 +19,22 @@ router.get('/', function(req, res, next) {
     res.send(highlight);
   });
 });
+
+/*
+ * POST save highlight info to DB
+ * Parameters: video_id, start, end, highlight_url
+ */
+router.post('/save', function(req, res, next) {
+  var newHighlight = new Highlight({
+    video_id: req.body.video_id,
+    start: req.body.start,
+    end: req.body.end,
+    highlight_url: req.body.highlight_url,
+  });
+
+  newHighlight.save();
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end();
+}
 
 module.exports = router;
