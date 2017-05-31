50ed4ca3c53a09eb4a3645bbf0edd5956c7ff279
Jaewon Jung <wjdwodnjs147@kaist.ac.kr>
2017-05-31 20:31:28 +0900

    Fix indentation in highlight.js

--- routes/highlight.js
+++ routes/highlight.js
@@ -11,30 +11,35 @@ var Highlight = require('../models/Highlight');
  * Query: video_id (?video_id=xxxxx)
  */
 router.get('/', function(req, res, next) {
-  Highlight.findOne({
-    video_id: req.query.video_id
-  }).
-  exec(function (err, highlight) {
-    if (err) return console.log(err);
-    res.send(highlight);
-  });
+    generate_highlight();
+    Highlight.findOne({
+        video_id: req.query.video_id
+    }).
+    exec(function (err, highlight) {
+        if (err) return console.log(err);
+        res.send(highlight);
+    });
 });
+
+function generate_highlight() {
+
+}
 
 /*
  * POST save highlight info to DB
  * Parameters: video_id, start, end, highlight_url
  */
 router.post('/save', function(req, res, next) {
-  var newHighlight = new Highlight({
-    video_id: req.body.video_id,
-    start: req.body.start,
-    end: req.body.end,
-    highlight_url: req.body.highlight_url,
-  });
+    var newHighlight = new Highlight({
+        video_id: req.body.video_id,
+        start: req.body.start,
+        end: req.body.end,
+        highlight_url: req.body.highlight_url,
+    });
 
-  newHighlight.save();
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end();
-}
+    newHighlight.save();
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end();
+});
 
 module.exports = router;
