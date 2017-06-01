56ef9ad04d1a0b1afef793242cd8dcf6b356e3b0
hyunsungcho <hscho1226@gmail.com>
2017-06-02 03:39:42 +0900

    Set index true for entries

--- models/Highlight.js
+++ models/Highlight.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var highlightSchema = new Schema({
-  video_id: {type: String, required: true},
-  start: {type: Number, required: true},
-  end: {type: Number, required: true}
+  video_id: {type: String, required: true, index: true},
+  start: {type: Number, required: true, index: true},
+  end: {type: Number, required: true, index: true},
+  highlight_url: {type: String}
 });
 highlightSchema.index({video_id: 1, start: 1, end: 1}, {unique: true});
 
